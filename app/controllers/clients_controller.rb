ccaf1a28b96279cef02c00be516f4b5afe68789c
Ben Christianson <thhuntertgm@gmail.com>
2014-10-30 14:35:25 -0500

    Clients controller set up.

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,26 +1,47 @@
 class ClientsController < ApplicationController
 
-before_action :set_client, only: [:show, :edit, :update, :destroy]
+  before_action :set_client, only: [:show, :edit, :update, :destroy]
 
-def index
-  @clients = Client.all
-end
+  def index
+    @clients = Client.all
+  end
 
-def new
-  @client = Client.new
-end
+  def new
+    @client = Client.new
+  end
 
-def create
-  @client = Client.new(client_params)
+  def create
+    @client = Client.new(client_params)
+    if @client.save
+      flash[:success] = "Client created successfully."
+      redirect_to clients_path
+    else
+      render 'new'
+    end
+  end
 
-end
+  def edit
+  end
+
+  def update
+    if @client.update(client_params)
+      redirect_to clients_path, notice: "Client ##{@client.id} was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @client.destroy
+      redirect_to clients_path, notice: "Client ##{@client.id} was successfully deleted."
+  end
 
   private
     def set_client
       @client = Client.find(params[:id])
     end
 
-    def item_params
+    def client_params
       params.require(:client).permit(:last_name)
     end
 
--- app/models/client.rb
+++ app/models/client.rb
@@ -3,4 +3,6 @@ class Client < ActiveRecord::Base
   has_many :behaviors
 
   attr_encryptor :last_name, key: Rails.application.secrets.client_last_name_key
+
+  validates :last_name, presence: true
 end

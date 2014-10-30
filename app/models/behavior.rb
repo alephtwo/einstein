d4183c44536f1ab0151574e4ed6926794334650a
Ben Christianson <thhuntertgm@gmail.com>
2014-10-30 13:37:13 -0500

    Added attr_encryptor.

--- app/models/behavior.rb
+++ app/models/behavior.rb
@@ -1,2 +1,3 @@
 class Behavior < ActiveRecord::Base
+  attr_encryptor :description, key: Rails.application.secrets.behavior_description_key
 end
--- app/models/client.rb
+++ app/models/client.rb
@@ -1,2 +1,3 @@
 class Client < ActiveRecord::Base
+  attr_encryptor :last_name, key: Rails.application.secrets.client_last_name_key
 end

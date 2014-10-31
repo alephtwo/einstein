88fcb9ac5e12f4ada25ffdd60d19572efae5429a
Ben Christianson <thhuntertgm@gmail.com>
2014-10-31 14:46:05 -0500

    Behavior scaffold complete.

--- app/controllers/behaviors_controller.rb
+++ app/controllers/behaviors_controller.rb
@@ -1,7 +1,52 @@
 class BehaviorsController < ApplicationController
 
-def index
-  @behaviors = Behavior.all
-end
+  before_action :set_behavior, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @behaviors = Behavior.all
+  end
+
+  def show
+  end
+
+  def new
+    @behavior = Behavior.new
+  end
+
+  def create
+    @behavior = Behavior.new(behavior_params)
+    if @behavior.save
+      flash[:success] = "Behavior created successfully."
+      redirect_to behaviors_path
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @behavior.update(behavior_params)
+      redirect_to behaviors_path, notice: "Behavior was successfully updated."
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @behavior.destroy
+    redirect_to behaviors_path
+  end
+
+  private
+
+    def set_behavior
+      @behavior = Behavior.find(params[:id])
+    end
+
+    def behavior_params
+      params.require(:behavior).permit(:description, :client_id)
+    end
 
 end

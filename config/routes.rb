ec06f0809603e2ec33d40d6023960350e27f7f18
Ben Christianson <thhuntertgm@gmail.com>
2014-11-02 14:53:11 -0600

    Removed unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   resources :employees
   resources :clients
   resources :behaviors, except: [:show, :new, :index]
-  resources :behavior_reports, except: [:show, :index]
+  resources :behavior_reports, except: [:show, :index, :edit, :update]
 
 end

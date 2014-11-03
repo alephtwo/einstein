b7008dd67f4e3c642f6b4622110a7b50aec8e5fc
Ben Christianson <thhuntertgm@gmail.com>
2014-11-02 21:07:16 -0600

    Redirect to clients path on successful login.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include ClientSessionsHelper
+
+  def after_sign_in_path_for(resource)
+    clients_path
+  end
 end

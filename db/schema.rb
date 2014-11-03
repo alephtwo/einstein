c0bfe0e761242e8ca0453244437bf9fe63347dc4
Ben Christianson <thhuntertgm@gmail.com>
2014-11-02 20:31:13 -0600

    Usernames instead of emails on users.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141103020947) do
+ActiveRecord::Schema.define(version: 20141103021915) do
 
   create_table "behavior_reports", force: true do |t|
     t.integer  "behavior_id"
@@ -51,9 +51,11 @@ ActiveRecord::Schema.define(version: 20141103020947) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "is_admin",               default: false
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end

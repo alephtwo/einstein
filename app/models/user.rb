1aa6c0b70053da1d654d1dafd7a24873031f9be8
Ben Christianson <thhuntertgm@gmail.com>
2014-11-02 21:51:11 -0600

    Users emails aren't encrypted.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  attr_encryptor :email, key: Rails.application.secrets.user_email_key
+
 end

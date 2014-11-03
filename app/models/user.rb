46a33264f0415e9e012bb0fc947fdcf7473a0df2
Ben Christianson <thhuntertgm@gmail.com>
2014-11-02 22:44:03 -0600

    Removed email encryptor from user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,4 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
 
-  attr_encryptor :email, key: Rails.application.secrets.user_email_key
-
 end

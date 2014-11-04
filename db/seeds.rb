7d1d6f6e9ae8008954bc20110868dde3e6296217
Ben Christianson <thhuntertgm@gmail.com>
2014-11-04 16:20:51 -0600

    office@caillierclinic

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-User.create(email: "ben.christianson@uwalumni.com", password: "administrator", password_confirmation: "administrator", is_admin: true)
+User.create(email: "office@caillierclinic.com", password: "administrator", password_confirmation: "administrator", is_admin: true)

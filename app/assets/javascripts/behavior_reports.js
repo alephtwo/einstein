2567e899bc9e0dd4dbf8f0a38826974d4aadd25b
Ben Christianson <thhuntertgm@gmail.com>
2014-11-06 12:14:23 -0600

    No more disabling of buttons

--- app/assets/javascripts/behavior_reports.js
+++ app/assets/javascripts/behavior_reports.js
@@ -2,5 +2,4 @@ $("#submit-report").click(function(){
 	$(this).addClass("disabled");
 	$(this).toggleClass("secondary");
 	$(this).val("Please wait...");
-	$(this).attr("disabled", "disabled");
 });

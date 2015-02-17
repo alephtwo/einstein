74f0c362e62ec796f8aea9cc8ea774cf8be77b12
Ben Christianson <thhuntertgm@gmail.com>
2015-02-17 19:10:12 +0000

    Removed after_sign_out path for Devise.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,9 +7,4 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     clients_path
   end
-
-  def after_sign_out_path_for(resource)
-    new_user_session_path
-  end
-
 end

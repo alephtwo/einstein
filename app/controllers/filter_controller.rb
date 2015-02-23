478420ab14729a0801da2ad810e675f7bf877fff
Ben Christianson <thhuntertgm@gmail.com>
2015-02-23 22:36:01 +0000

    Moved browser string logging to controller.

--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -1,6 +1,7 @@
 class FilterController < ApplicationController
 
   def index
+    logger.info("[BROWSER]  #{request.env['HTTP_USER_AGENT']}")
     if user_signed_in? && !client_signed_in?
       redirect_to clients_path
     elsif !user_signed_in? && client_signed_in?

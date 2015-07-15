c13c64a9bcc3717510ae0679502e32171331b7b6
Ben Christianson <thhuntertgm@gmail.com>
2015-07-14 19:34:01 -0500

    Behavior Reports controller has been rubocopped

--- app/controllers/behavior_reports_controller.rb
+++ app/controllers/behavior_reports_controller.rb
@@ -1,5 +1,5 @@
+# Behavior Reports Controller
 class BehaviorReportsController < ApplicationController
-
   before_filter :check_client_login, only: [:new, :create]
   before_filter :check_user, only: [:destroy, :edit, :update, :remove]
   before_action :set_report, only: [:destroy, :edit, :update, :remove]
@@ -7,7 +7,9 @@ class BehaviorReportsController < ApplicationController
   def index
     @behavior_reports = BehaviorReport.all
     respond_to do |format|
-      format.xlsx { render xlsx: "index", filename: "behavior_reports_#{Time.now.to_i}.xlsx"  }
+      format.xlsx do
+        render xlsx: 'index', filename: "behavior_reports_#{Time.now.to_i}.xlsx"
+      end
     end
   end
 
@@ -19,10 +21,10 @@ class BehaviorReportsController < ApplicationController
   def create
     @behavior_report = BehaviorReport.new(behavior_report_params)
     if @behavior_report.save
-      flash[:success] = "Report submitted successfully."
+      flash[:success] = 'Report submitted successfully.'
       redirect_to submit_path
     else
-      flash.now[:alert] = "Report could not be submitted."
+      flash.now[:alert] = 'Report could not be submitted.'
       render 'new'
     end
   end
@@ -32,14 +34,17 @@ class BehaviorReportsController < ApplicationController
 
   def update
     if @behavior_report.update(behavior_report_params)
-      redirect_to @behavior_report.behavior.client, notice: "Behavior report was successfully updated."
+      redirect_to(
+        @behavior_report.behavior.client,
+        notice: 'Behavior report was successfully updated.'
+      )
     else
       render :edit
     end
   end
 
   def remove
-    @behavior_report.update(:removed => "true")
+    @behavior_report.update(removed: true)
     redirect_to @behavior_report.behavior.client
   end
 
@@ -51,25 +56,21 @@ class BehaviorReportsController < ApplicationController
 
   private
 
-    def check_client_login
-      unless client_signed_in?
-        redirect_to login_path
-      end
-    end
+  def check_client_login
+    redirect_to login_path unless client_signed_in?
+  end
 
-    def check_user
-      unless user_signed_in?
-        flash[:error] = "You do not have access to that page."
-        redirect_to root_path
-      end
-    end
+  def check_user
+    return if user_signed_in?
+    flash[:error] = 'You do not have access to that page.'
+    redirect_to root_path
+  end
 
-    def set_report
-      @behavior_report = BehaviorReport.find(params[:id])
-    end
+  def set_report
+    @behavior_report = BehaviorReport.find(params[:id])
+  end
 
-    def behavior_report_params
-      params.require(:behavior_report).permit(:behavior_id, :occurrences)
-    end
-
+  def behavior_report_params
+    params.require(:behavior_report).permit(:behavior_id, :occurrences)
+  end
 end

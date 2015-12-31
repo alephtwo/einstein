2d3b5adbc2ba8e74d8c6d62910b5927214c8213c
Ben Christianson <thhuntertgm@gmail.com>
2015-12-30 23:06:56 -0600

    Modified application_controller to (hopefully) fix CSRF bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 # Application level controller
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  skip_before_filter :verify_authenticity_token, if: -> { controller_name == 'sessions' && action_name == 'create' }
   include ClientSessionsHelper
 
   def after_sign_in_path_for(_resource)

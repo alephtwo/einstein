7f1effb60bba79f52d1473f68013e39e417e01a5
Ben Christianson <thhuntertgm@gmail.com>
2016-01-28 11:31:38 -0600

    Fixed a typo on creation of a lambda

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,8 @@
 # Application level controller
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  skip_before_filter :verify_authenticity_token, if: lambda do
-    controller_name == 'sessions' && action_name == 'create'
-  end
+  skip_cond = -> { controller_name == 'sessions' && action_name == 'create' }
+  skip_before_filter :verify_authenticity_token, if: skip_cond
   include ClientSessionsHelper
 
   def after_sign_in_path_for(_resource)
